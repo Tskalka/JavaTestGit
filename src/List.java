1fe7e808c8107b4e40ce6532815b56f06bb25720
Tskalka <tyler.skalka@gmail.com>
2023-02-23 10:41:05 -0700

    Created and changed classes. Add object creation from those classes

---
+++ src/List.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class List {
+    private ArrayList<Integer> a = new ArrayList<Integer>();
+
+
+    public void add(int num)
+    {
+        a.add(num);
+    }
+
+    public ArrayList<Integer> getArray()
+    {
+        return a;
+    }
+}
--- src/Output.java
+++ src/Output.java
@@ -1,2 +1,22 @@
 public class Output {
+
+    // Variable types, red means native to java,
+    // non-colored is an object, access object methods with a .
+    public String hello = "hi";
+
+    public String returnOutput()
+    {
+        return "output";
+    }
+
+    public void addExclamationPoint(String s)
+    {
+        System.out.println(s + "!");
+    }
+
+    public String addQuestionMark(String s)
+    {
+        return s + "?";
+    }
+
 }
