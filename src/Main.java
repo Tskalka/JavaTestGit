e8a800c5bb287e6829bb4c7882012b902f3d1627
Tskalka <tyler.skalka@gmail.com>
2023-02-23 09:54:33 -0700

    Added varaibles and outputs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* Every line of code must be inside a "class"!
+           Name of java file must match class name */
+
+        // Variable types
+        String Hello = "Hi";
+        int number = 3;
+        float decimole = 1.0F;
+        char letter = 'a';
+        boolean isFun = true;
+
+        System.out.println(Hello);
+        System.out.println(number);
+        System.out.println(decimole);
     }
 }
